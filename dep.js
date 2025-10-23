0d953aeecd4ee4e637d965c570fb0a9721a1a83d
Sinewyk <sinewyk@gmail.com>
2025-10-23 15:42:14 +0200

    confirm that only spread syntax is the issue

--- dep.js
+++ dep.js
@@ -1,3 +1,4 @@
 const someArray = [0, 1, 2, 3];
 
+export const [A, B] = someArray;
 export const [FIRST, ...REST] = someArray;
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-import { FIRST, REST } from "./dep.js";
+import { A, B, FIRST, REST } from "./dep.js";
 
+console.log(A, B);
 console.log(FIRST, REST);
